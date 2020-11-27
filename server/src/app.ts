2a91d0eea8d785f071852b25208e61b36f4f2d1b
Davi Oliveira <davioliveira.java@gmail.com>
2020-11-26 21:20:37 -0300

    [wip] wip queue config

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,13 @@
-import express from 'express';
 import cors from 'cors';
+import express from 'express';
+import queueConfig from './config/queueConfig';
 import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(queueConfig);
 app.use(routes);
 
 export default app;
