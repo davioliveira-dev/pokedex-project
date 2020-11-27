3e107c1ee08433f78136df63a49c7fbbf779983f
Davi Oliveira <davioliveira.java@gmail.com>
2020-11-26 21:24:01 -0300

    [del] deleted pokemon controller (no longer useful)

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,14 +1,12 @@
-import {Router} from 'express';
 import AuthController from './controllers/AuthController';
-import PokemonsController from './controllers/PokemonsController';
+import {Router} from 'express';
 import UserController from './controllers/UserController';
 import authMiddleware from './middlewares/authMiddleware';
 
 const routes = Router();
 
 routes.post('/users', UserController.store);
-routes.get('/users', authMiddleware, UserController.index);
 routes.post('/auth', AuthController.authenticate);
-routes.get('/pokemons', PokemonsController.index);
+routes.get('/users', authMiddleware, UserController.index);
 
 export default routes;
